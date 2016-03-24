49ec9c77853aa81f68e86b8d56bc72554c4743f2
Matthew Skomarovsky <matthew@verypolite.com>
2016-03-24 14:58:37 -0400

    added book navigation test

--- tests/browser/navigate.js
+++ tests/browser/navigate.js
@@ -1,9 +1,8 @@
 signIn = require("./lib/signIn");
 
 module.exports = {
-  "navigate to lane" : function (browser) {
+  "navigate to lane" : function(browser) {
     var laneSelector = "li:first-child .lane h2 a";
-    var homeTitle;
 
     signIn(browser)
       .waitForElementVisible(laneSelector, 1000)
@@ -16,6 +15,25 @@ module.exports = {
             .waitForElementNotPresent("h1.loading", 2000)
             .assert.urlEquals(laneUrl)
             .assert.titleContains(laneTitle);
+        });
+      })
+      .end();
+  },
+  "navigate to book in lane": function(browser) {
+    var bookSelector = "ul.laneBooks li:first-child a.laneBookLink";
+
+    signIn(browser)
+      .waitForElementVisible(bookSelector, 1000)
+      .getAttribute(bookSelector, "href", function(result) {
+        var bookUrl = result.value;
+        this.getText(bookSelector, function(result) {
+          var bookTitle = result.value;
+          this
+            .click(bookSelector)
+            .waitForElementPresent("h1.bookDetailsTitle", 2000)
+            .assert.urlEquals(bookUrl)
+            .assert.titleContains(bookTitle);
+          this.expect.element("h1.bookDetailsTitle").text.to.equal(bookTitle);
         });
       })
       .end();
