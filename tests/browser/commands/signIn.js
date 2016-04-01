cff6bef2aa54bbbb0e8ce1ab509f044e617e5f10
Matthew Skomarovsky <matthew@verypolite.com>
2016-04-01 13:49:46 -0400

    signIn command uses config

--- tests/browser/commands/signIn.js
+++ tests/browser/commands/signIn.js
@@ -2,10 +2,10 @@ exports.command = function() {
   return this
     .goHome()
     .waitForElementVisible("input#Email", 1000)
-    .setValue("input#Email", "librarysimplifiedtesting")
+    .setValue("input#Email", this.globals.username)
     .click("input#next")
     .waitForElementVisible("input#Passwd", 1000)
-    .setValue("input#Passwd", "seas37?spays")
+    .setValue("input#Passwd", this.globals.password)
     .click("input#signIn")
     .waitForElementVisible("button#submit_approve_access:enabled", 10000)
     .click("button#submit_approve_access")
