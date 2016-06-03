4efb6f64913a975ffbdfde7aa527510d262f9e75
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-06-03 15:20:33 -0400

    added Dashboard tests

---
+++ src/components/__tests__/Dashboard-test.tsx
@@ -0,0 +1,31 @@
+jest.autoMockOff();
+
+import * as React from "react";
+import { shallow } from "enzyme";
+
+import Dashboard from "../Dashboard";
+import Header from "../Header";
+import CirculationEvents from "../CirculationEvents";
+import buildStore from "../../store";
+
+describe("Dashboard", () => {
+  let wrapper;
+  let store;
+  let context;
+
+  beforeEach(() => {
+    store = buildStore();
+    context = { editorStore: store };
+    wrapper = shallow(<Dashboard />, { context });
+  });
+
+  it("shows Header", () => {
+    let header = wrapper.find(Header);
+    expect(header.length).toBe(1);
+  });
+
+  it("shows CirculationEvents", () => {
+    let events = wrapper.find(CirculationEvents);
+    expect(events.prop("store")).toBe(store);
+  });
+});
