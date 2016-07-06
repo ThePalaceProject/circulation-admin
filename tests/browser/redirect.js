79a9629604fc683157b68550d0afd072d753e52e
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-07-06 13:43:08 -0400

    added browser redirect test

---
+++ tests/browser/redirect.js
@@ -0,0 +1,50 @@
+var http = require("http");
+
+module.exports = {
+  "attempt to view a book before signing in": function(browser) {
+    // before signing in, find a book url in the catalog feed
+    http.get({
+      host: "localhost",
+      port: 6500,
+      path: "/groups/"
+    }, function(response) {
+      var body = "";
+
+      response.on("data", function(chunk) {
+        body += chunk;
+      });
+
+      response.on("end", function() {
+        var bookTitle = body.match(/<entry [\s\S]+?<title>([^<]+)<\/title>/i)[1];
+        var link = body.match(/<link [^>]*rel="alternate[^>]+\/>/i)[0];
+        var bookUrl = link.match(/href="([^"]+)"/)[1];
+
+        // transform the book url into an admin book url
+        var adminBookUrl =
+          browser.globals.homeUrl + "book/" +
+          encodeURIComponent(
+            bookUrl.replace(browser.globals.entryBaseUrl, "")
+          );
+
+        browser
+          .url(adminBookUrl)
+          .waitForElementVisible("input#Email", 1000)
+          .assert.urlContains(encodeURIComponent("state=") + encodeURI(encodeURI(adminBookUrl)))
+          .setValue("input#Email", browser.globals.username)
+          .click("input#next")
+          .waitForElementVisible("input#Passwd", 1000)
+          .setValue("input#Passwd", browser.globals.password)
+          .click("input#signIn")
+          .waitForElementVisible("button#submit_approve_access:enabled", 10000)
+          .click("button#submit_approve_access")
+          .waitForElementVisible("nav", 1000)
+          .assert.urlContains(adminBookUrl)
+          .assert.containsText("h1.bookDetailsTitle", bookTitle);
+      });
+    });
+  },
+
+  after: function(browser) {
+    browser.end();
+  }
+};
