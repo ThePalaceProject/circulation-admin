f1e4c384d2b1617236a71ab36d1e94ed48922d70
Edwin Guzman <edwin.gzmn@gmail.com>
2018-02-12 12:22:00 -0500

    Fixing search/storage names in reducers, and updating version.

--- src/reducers/storageServices.ts
+++ src/reducers/storageServices.ts
@@ -2,4 +2,4 @@ import { StorageServicesData } from "../interfaces";
 import ActionCreator from "../actions";
 import createFetchEditReducer from "./createFetchEditReducer";
 
-export default createFetchEditReducer<StorageServicesData>(ActionCreator.SEARCH_SERVICES, ActionCreator.EDIT_SEARCH_SERVICE);
+export default createFetchEditReducer<StorageServicesData>(ActionCreator.STORAGE_SERVICES, ActionCreator.EDIT_STORAGE_SERVICE);
