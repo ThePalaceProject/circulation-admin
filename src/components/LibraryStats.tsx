c5c60b2290e3d3ec9aef2d23f3bda664e03f3250
Amy Slagle <amslagle@gmail.com>
2018-07-24 22:29:16 -0400

    Changed stat groups and single stats to lists.

--- src/components/LibraryStats.tsx
+++ src/components/LibraryStats.tsx
@@ -35,58 +35,64 @@ export default class LibraryStats extends React.Component<LibraryStatsProps, voi
           <h2>Statistics for All Libraries</h2>
         }
         { this.props.stats &&
-          <div className="stats">
+          <ul className="stats">
             { this.props.stats.patrons.total > 0 &&
-              <div className="stat-group">
+              <li className="stat-group">
                 <h3><span className="stat-grouping-label">Patrons</span></h3>
-                <div className="single-stat">
-                  <span className="stat-value">{this.formatNumber(this.props.stats.patrons.total)}</span>
-                  <span className="stat-label">Total Patrons</span>
-                </div>
-                <div className="single-stat">
-                  <span className="stat-value">{this.formatNumber(this.props.stats.patrons.with_active_loans)}</span>
-                  <span className="stat-label">Patrons With Active Loans</span>
-                </div>
-                <div className="single-stat">
-                  <span className="stat-value">{this.formatNumber(this.props.stats.patrons.with_active_loans_or_holds)}</span>
-                  <span className="stat-label">Patrons With Active Loans or Holds</span>
-                </div>
-              </div>
+                <ul>
+                  <li className="single-stat">
+                    <span className="stat-value">{this.formatNumber(this.props.stats.patrons.total)}</span>
+                    <span className="stat-label">Total Patrons</span>
+                  </li>
+                  <li className="single-stat">
+                    <span className="stat-value">{this.formatNumber(this.props.stats.patrons.with_active_loans)}</span>
+                    <span className="stat-label">Patrons With Active Loans</span>
+                  </li>
+                  <li className="single-stat">
+                    <span className="stat-value">{this.formatNumber(this.props.stats.patrons.with_active_loans_or_holds)}</span>
+                    <span className="stat-label">Patrons With Active Loans or Holds</span>
+                  </li>
+                </ul>
+              </li>
             }
             { this.props.stats.patrons.total > 0 &&
-              <div className="stat-group">
+              <li className="stat-group">
                 <h3><span className="stat-grouping-label">Circulation</span></h3>
-                <div className="single-stat">
-                  <div className="stat-value">{this.formatNumber(this.props.stats.patrons.loans)}</div>
-                  <div className="stat-label">Active Loans</div>
-                </div>
-                <div className="single-stat">
-                  <div className="stat-value">{this.formatNumber(this.props.stats.patrons.holds)}</div>
-                  <div className="stat-label">Active Holds</div>
-                </div>
-              </div>
+                <ul>
+                  <li className="single-stat">
+                    <div className="stat-value">{this.formatNumber(this.props.stats.patrons.loans)}</div>
+                    <div className="stat-label">Active Loans</div>
+                  </li>
+                  <li className="single-stat">
+                    <div className="stat-value">{this.formatNumber(this.props.stats.patrons.holds)}</div>
+                    <div className="stat-label">Active Holds</div>
+                  </li>
+                </ul>
+              </li>
             }
-            <div className="stat-group">
+            <li className="stat-group">
               <h3><span className="stat-grouping-label">Inventory</span></h3>
-              <div className="single-stat">
-                <div className="stat-value">{this.formatNumber(this.props.stats.inventory.titles)}</div>
-                <div className="stat-label">Titles</div>
-              </div>
-              { this.props.stats.inventory.licenses > 0 &&
-                <div className="single-stat">
-                  <div className="stat-value">{this.formatNumber(this.props.stats.inventory.licenses)}</div>
-                  <div className="stat-label">Total Licenses</div>
-                </div>
-              }
-              { this.props.stats.inventory.licenses > 0 &&
-                <div className="single-stat">
-                  <div className="stat-value">{this.formatNumber(this.props.stats.inventory.available_licenses)}</div>
-                  <div className="stat-label">Available Licenses</div>
-                </div>
-              }
-            </div>
+              <ul>
+                <li className="single-stat">
+                  <div className="stat-value">{this.formatNumber(this.props.stats.inventory.titles)}</div>
+                  <div className="stat-label">Titles</div>
+                </li>
+                { this.props.stats.inventory.licenses > 0 &&
+                  <li className="single-stat">
+                    <div className="stat-value">{this.formatNumber(this.props.stats.inventory.licenses)}</div>
+                    <div className="stat-label">Total Licenses</div>
+                  </li>
+                }
+                { this.props.stats.inventory.licenses > 0 &&
+                  <li className="single-stat">
+                    <div className="stat-value">{this.formatNumber(this.props.stats.inventory.available_licenses)}</div>
+                    <div className="stat-label">Available Licenses</div>
+                  </li>
+                }
+              </ul>
+            </li>
             { collectionCounts.length > 0 &&
-              <div className="stat-group stat-group-wide">
+              <li className="stat-group stat-group-wide">
                 <h3><span className="stat-grouping-label">Collections</span></h3>
                 <BarChart width={collectionCounts.length * 100} height={350} data={collectionCounts}>
                   <XAxis dataKey="label" interval={0} angle={-45} textAnchor="end" padding={{ left: 50, right: 50 }} height={175} />
@@ -95,9 +101,9 @@ export default class LibraryStats extends React.Component<LibraryStatsProps, voi
                   <Bar stackId="collections" dataKey="Licensed Titles" barSize={50} fill="#737373" />
                   <Bar stackId="collections" dataKey="Open Access Titles" barSize={50} fill="#000000" />
                 </BarChart>
-              </div>
+              </li>
             }
-          </div>
+          </ul>
         }
       </div>
     );
