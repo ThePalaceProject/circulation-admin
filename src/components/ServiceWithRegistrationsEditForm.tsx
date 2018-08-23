4aa5b6b2e2f56a7c74815648802cf5b01ec697b8
Edwin Guzman <edwin.gzmn@gmail.com>
2018-08-23 12:07:47 -0400

    First stab at adding select dropdown for registry selection.

--- src/components/ServiceWithRegistrationsEditForm.tsx
+++ src/components/ServiceWithRegistrationsEditForm.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import ServiceEditForm from "./ServiceEditForm";
+import EditableInput from "./EditableInput";
 import { ServicesWithRegistrationsData } from "../interfaces";
 
 /** Form for editing discovery services on the discovery service configuration tab.
@@ -19,51 +20,65 @@ export default class ServiceWithRegistrationsEditForm<T extends ServicesWithRegi
         { this.props.item && this.protocolSupportsRegistration() && this.props.data && this.props.data.allLibraries && this.props.data.allLibraries.length > 0 &&
           <div>
             <h2>Register libraries</h2>
-            { this.props.data.allLibraries.map(library =>
-                <div className="service-with-registrations-library" key={library.short_name}>
-                  { this.getRegistrationStatus(library) === "success" &&
-                    <div>
-                      { library.name }
-                      <span className="bg-success">
-                        Registered
-                      </span>
-                      <button
-                        type="button"
-                        className="btn btn-default"
-                        disabled={this.props.disabled}
-                        onClick={() => this.context.registerLibrary(library)}
-                        >Update registration</button>
-                    </div>
-                  }
-                  { this.getRegistrationStatus(library) === "failure" &&
-                    <div>
-                      { library.name }
-                      <span className="bg-danger">
-                        Registration failed
-                      </span>
-                      <button
-                        type="button"
-                        className="btn btn-default"
-                        disabled={this.props.disabled}
-                        onClick={() => this.context.registerLibrary(library)}
-                        >Retry registration</button>
-                    </div>
-                  }
-                  { this.getRegistrationStatus(library) === null &&
-                    <div>
-                      { library.name }
-                      <span className="bg-warning">
-                        Not registered
-                      </span>
-                      <button
-                        type="button"
-                        className="btn btn-default"
-                        disabled={this.props.disabled}
-                        onClick={() => this.context.registerLibrary(library)}
-                        >Register</button>
-                    </div>
-                  }
-                </div>
+            { this.props.data.allLibraries.map(library => {
+                const registryStage = this.getStageStatus(library);
+                return (
+                  <div className="service-with-registrations-library" key={library.short_name}>
+                    { library.name }
+                    <EditableInput
+                      elementType="select"
+                      disabled={registryStage === "production"}
+                      readOnly={registryStage === "production"}
+                      name="registryStage"
+                      label="Stage"
+                      value={registryStage}
+                      ref="protocol"
+                      onChange={this.handleProtocolChange}
+                    >
+                      <option value="testing">Testing</option>
+                      <option value="production">Production</option>
+                    </EditableInput>
+                    { this.getRegistrationStatus(library) === "success" &&
+                      <div>
+                        <span className="bg-success">
+                          Registered
+                        </span>
+                        <button
+                          type="button"
+                          className="btn btn-default"
+                          disabled={this.props.disabled}
+                          onClick={() => this.context.registerLibrary(library)}
+                          >Update registration</button>
+                      </div>
+                    }
+                    { this.getRegistrationStatus(library) === "failure" &&
+                      <div>
+                        <span className="bg-danger">
+                          Registration failed
+                        </span>
+                        <button
+                          type="button"
+                          className="btn btn-default"
+                          disabled={this.props.disabled}
+                          onClick={() => this.context.registerLibrary(library)}
+                          >Retry registration</button>
+                      </div>
+                    }
+                    { this.getRegistrationStatus(library) === null &&
+                      <div>
+                        <span className="bg-warning">
+                          Not registered
+                        </span>
+                        <button
+                          type="button"
+                          className="btn btn-default"
+                          disabled={this.props.disabled}
+                          onClick={() => this.context.registerLibrary(library)}
+                          >Register</button>
+                      </div>
+                    }
+                  </div>);
+                }
               )
             }
           </div>
@@ -80,6 +95,21 @@ export default class ServiceWithRegistrationsEditForm<T extends ServicesWithRegi
         for (const libraryInfo of serviceInfo.libraries) {
           if (libraryInfo.short_name === library.short_name) {
             return libraryInfo.status;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  getStageStatus(library): string | null {
+    const serviceId = this.props.item && this.props.item.id;
+    const libraryRegistrations = this.props.data && this.props.data.libraryRegistrations || [];
+    for (const serviceInfo of libraryRegistrations) {
+      if (serviceInfo.id === serviceId) {
+        for (const libraryInfo of serviceInfo.libraries) {
+          if (libraryInfo.short_name === library.short_name) {
+            return libraryInfo.stage;
           }
         }
       }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -328,6 +328,7 @@ export interface DiscoveryServicesData extends ServicesWithRegistrationsData {
 
 export interface LibraryDataWithStatus extends LibraryData {
   status: string;
+  stage?: string;
 }
 
 export interface LibraryRegistrationData {
