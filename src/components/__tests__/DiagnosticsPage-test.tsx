4f0dda6e6add62841d960059ffbe50ee2fd54f82
Adriana Alter <adrianaalter@nypl.org>
2019-02-08 13:53:21 -0500

    Remove unused import

--- src/components/__tests__/DiagnosticsPage-test.tsx
+++ src/components/__tests__/DiagnosticsPage-test.tsx
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 
 import * as React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 
 import DiagnosticsPage from "../DiagnosticsPage";
 import DiagnosticsTabContainer from "../DiagnosticsTabContainer";
