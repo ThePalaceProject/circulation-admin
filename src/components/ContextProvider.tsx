4a58305f6b8e8188dd32e9487d233a4f35e85bec
Adriana Alter <adrianaalter@nypl.org>
2019-04-19 16:03:15 -0400

    Revert unnecessary change

--- src/components/ContextProvider.tsx
+++ src/components/ContextProvider.tsx
@@ -18,7 +18,7 @@ export interface ContextProviderProps extends React.Props<any> {
 
 /** Provides a redux store, configuration options, and a function to create URLs
     as context to admin interface pages. */
-export default class ContextProvider extends React.Component<ContextProviderProps, {}> {
+export default class ContextProvider extends React.Component<ContextProviderProps, void> {
   store: Store<State>;
   admin: Admin;
   pathFor: PathFor;
