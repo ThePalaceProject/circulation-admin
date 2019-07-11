58bed4137a5d0100241c09dc68551e38df2f605a
Edwin Guzman <edwin.gzmn@gmail.com>
2019-07-11 12:51:35 -0400

    Updating type for buildStore.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, Store, Reducer } from "redux";
 import catalogReducers from "opds-web-client/lib/reducers/index";
-import editorReducers from "./reducers/index";
+import editorReducers, { State } from "./reducers/index";
 const thunk = require("redux-thunk").default;
 
 let reducers: Reducer = combineReducers({
@@ -10,7 +10,7 @@ let reducers: Reducer = combineReducers({
 
 /** Build a redux store with reducers specific to the admin interface
     as well as reducers from opds-web-client. */
-export default function buildStore(initialState?: any): Store<any> {
+export default function buildStore(initialState?: State): Store<State> {
   return createStore(
     reducers,
     initialState,
