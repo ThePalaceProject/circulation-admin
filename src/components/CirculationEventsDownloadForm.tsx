46732730146231b54d8428faa7aef97937595662
Edwin Guzman <edwin.gzmn@gmail.com>
2019-09-11 10:32:20 -0400

    Update and testing Form

--- src/components/CirculationEventsDownloadForm.tsx
+++ src/components/CirculationEventsDownloadForm.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Modal } from "react-bootstrap";
-import { Button } from "library-simplified-reusable-components";
+import { Button, Form, Input } from "library-simplified-reusable-components";
+import EditableInput from "./EditableInput";
 import * as qs from "qs";
 
 export interface CirculationEventsDownloadFormProps extends React.Props<CirculationEventsDownloadForm> {
@@ -10,6 +11,9 @@ export interface CirculationEventsDownloadFormProps extends React.Props<Circulat
 
 /** Form for downloading CirculationEvents for a particular date. */
 export default class CirculationEventsDownloadForm extends React.Component<CirculationEventsDownloadFormProps, {}> {
+  private dateStart = React.createRef<Input>();
+  private dateEnd = React.createRef<Input>();
+
   constructor(props) {
     super(props);
     this.download = this.download.bind(this);
@@ -18,6 +22,26 @@ export default class CirculationEventsDownloadForm extends React.Component<Circu
   render(): JSX.Element {
     // date inputs require YYYY-MM-DD format
     let today = new Date().toISOString().split("T")[0];
+    const t = (<>
+      <fieldset>
+        <legend className="control-label">Configuration</legend>
+        <EditableInput
+          elementType="select"
+          ref="type"
+          name="type"
+          label="Event type"
+          placeholder=""
+        >
+          <option value="all" aria-selected={false}>All Events</option>
+          <option value="location" aria-selected={false}>Events by Location</option>
+        </EditableInput>
+        <Input className="date-input" label="test"
+          name="dateStart" id="dateStart" value={today}
+          ref={this.dateStart} type="date" />
+        <Input className=""
+          label="Locations (comma separated list)" name="locations" id="locations" />
+      </fieldset>
+      </>);
 
     return (
       <Modal
@@ -28,15 +52,13 @@ export default class CirculationEventsDownloadForm extends React.Component<Circu
           <Modal.Title>Download CSV</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="form-group">
-            <label className="conrol-label">Date</label>
-            <input
-              className="form-control"
-              type="date"
-              ref="date"
-              defaultValue={today} />
-          </div>
-          <Button callback={this.download} content="Download" className="left-align"/>
+          <Form
+            onSubmit={this.download}
+            buttonContent="Download"
+            buttonClass="left-align"
+            content={t}
+          />
+        {/* <Button callback={this.download} content="download" /> */}
         </Modal.Body>
         <Modal.Footer>
           <Button className="inverted left-align" callback={this.props.hide} content="Close" />
@@ -46,7 +68,14 @@ export default class CirculationEventsDownloadForm extends React.Component<Circu
   }
 
   download() {
-    let date = (this.refs as any).date.value || null;
+    // let date = (this.refs as any).dateStart.value || null;
+    // let dateEnd = (this.refs as any).dateEnd.value || null;
+    const dateStartRef = this.dateStart.current || null;
+    console.log((this.dateStart.current).getValue());
+    const date = dateStartRef && dateStartRef.getValue() || null;
+    // const dateEndRef = this.dateEnd.current || null;
+    // const dateEnd = dateEndRef && dateEndRef.getValue() || null;
+    console.log(date);
     let url = "/admin/bulk_circulation_events";
     let params = qs.stringify({ date }, { skipNulls: true });
 
@@ -54,9 +83,9 @@ export default class CirculationEventsDownloadForm extends React.Component<Circu
       url += "?" + params;
     }
 
-    let link = document.createElement("a");
-    link.href = url;
-    link.target = "_blank";
-    link.click();
+    // let link = document.createElement("a");
+    // link.href = url;
+    // link.target = "_blank";
+    // link.click();
   }
 }
