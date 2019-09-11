c4da3cc3a89d089f3fb0ab246c7cc2649aa9c93a
Edwin Guzman <edwin.gzmn@gmail.com>
2019-09-11 16:38:25 -0400

    Updating the UI for the dashboard report form.

--- src/components/CirculationEventsDownloadForm.tsx
+++ src/components/CirculationEventsDownloadForm.tsx
@@ -9,56 +9,40 @@ export interface CirculationEventsDownloadFormProps extends React.Props<Circulat
   hide: () => void;
 }
 
+export interface CirculationEventsDownloadFormState {
+  showLocationsInput: boolean;
+}
+
 /** Form for downloading CirculationEvents for a particular date. */
-export default class CirculationEventsDownloadForm extends React.Component<CirculationEventsDownloadFormProps, {}> {
-  private dateStart = React.createRef<Input>();
-  private dateEnd = React.createRef<Input>();
+export default class CirculationEventsDownloadForm extends React.Component<CirculationEventsDownloadFormProps, CirculationEventsDownloadFormState> {
+  private type = React.createRef<HTMLSelectElement>();
+  private dateStart = React.createRef<EditableInput>();
+  private dateEnd = React.createRef<EditableInput>();
+  private locations = React.createRef<EditableInput>();
 
   constructor(props) {
     super(props);
+    this.state = { showLocationsInput: false };
+
     this.download = this.download.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+    this.renderInputs = this.renderInputs.bind(this);
+    this.getRefValue = this.getRefValue.bind(this);
+    this.toggleLocationsInput = this.toggleLocationsInput.bind(this);
   }
 
   render(): JSX.Element {
-    // date inputs require YYYY-MM-DD format
-    let today = new Date().toISOString().split("T")[0];
-    const t = (<>
-      <fieldset>
-        <legend className="control-label">Configuration</legend>
-        <EditableInput
-          elementType="select"
-          ref="type"
-          name="type"
-          label="Event type"
-          placeholder=""
-        >
-          <option value="all" aria-selected={false}>All Events</option>
-          <option value="location" aria-selected={false}>Events by Location</option>
-        </EditableInput>
-        <Input className="date-input" label="test"
-          name="dateStart" id="dateStart" value={today}
-          ref={this.dateStart} type="date" />
-        <Input className=""
-          label="Locations (comma separated list)" name="locations" id="locations" />
-      </fieldset>
-      </>);
-
     return (
       <Modal
         className="circ-events-download-form"
         show={this.props.show}
-        onHide={this.props.hide}>
+        onHide={this.props.hide}
+      >
         <Modal.Header>
           <Modal.Title>Download CSV</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form
-            onSubmit={this.download}
-            buttonContent="Download"
-            buttonClass="left-align"
-            content={t}
-          />
-        {/* <Button callback={this.download} content="download" /> */}
+          {this.renderForm()}
         </Modal.Body>
         <Modal.Footer>
           <Button className="inverted left-align" callback={this.props.hide} content="Close" />
@@ -67,25 +51,133 @@ export default class CirculationEventsDownloadForm extends React.Component<Circu
     );
   }
 
+  /**
+   * Renders the main <form> component
+   */
+  renderForm() {
+    return (
+      <Form
+        onSubmit={this.download}
+        buttonContent="Download"
+        buttonClass="left-align"
+        content={this.renderInputs()}
+      />
+    );
+  }
+
+  /**
+   * Toggle the locations text input whenever "locations" is selected in the dropdown.
+   */
+  toggleLocationsInput() {
+    // select elements get the value from the React ref differently.
+    const type = this.type.current && this.type.current.value;
+    let showLocationsInput = false;
+    if (type === "locations") {
+      showLocationsInput = true;
+    }
+    this.setState({ showLocationsInput });
+  }
+
+  /**
+   * Render all the inputs for the form. The locations input is conditionally
+   * rendered if it is selected in the Event type dropdown.
+   */
+  renderInputs() {
+    const eventTypes = [
+      { id: "all", label: "All Events" },
+      { id: "locations", label: "Events by Location" },
+    ];
+    // date inputs require YYYY-MM-DD format
+    let today = new Date().toISOString().split("T")[0];
+
+    return (
+      <>
+        <fieldset>
+          <legend className="control-label visuallyHidden">Configuration</legend>
+          <label htmlFor="event-select">
+            Event Type
+            <select
+              className="form-control"
+              id="event-select"
+              ref={this.type}
+              name="type"
+              onChange={this.toggleLocationsInput}
+            >
+              {
+                eventTypes.map(event =>
+                  <option key={`${event.id}-option`} value={event.id} aria-selected={false}>
+                    {event.label}
+                  </option>
+                )
+              }
+            </select>
+          </label>
+          {
+            this.state.showLocationsInput && (
+              <EditableInput
+                ref={this.locations}
+                className=""
+                label="Locations"
+                description="Comma separated list of locations to gather events from."
+                name="locations"
+                id="locations"
+                optionalText={false}
+              />
+            )
+          }
+          <EditableInput
+            ref={this.dateStart}
+            name="dateStart"
+            label="Date Start"
+            type="date"
+            value={today}
+            className="date-input"
+            optionalText={false}
+          />
+          <EditableInput
+            ref={this.dateEnd}
+            name="dateEnd"
+            label="Date End"
+            type="date"
+            value={today}
+            className="date-input"
+            optionalText={false}
+          />
+        </fieldset>
+      </>
+    );
+  }
+
+  /**
+   * Simple way to get the value of a EditableInput ref.
+   */
+  getRefValue(ref: React.RefObject<EditableInput>) {
+    const current = ref.current || null;
+    const value = current && current.getValue() || null;
+    return value;
+  }
+
+  /**
+   * Send all the data and get back the report.
+   */
   download() {
-    // let date = (this.refs as any).dateStart.value || null;
-    // let dateEnd = (this.refs as any).dateEnd.value || null;
-    const dateStartRef = this.dateStart.current || null;
-    console.log((this.dateStart.current).getValue());
-    const date = dateStartRef && dateStartRef.getValue() || null;
-    // const dateEndRef = this.dateEnd.current || null;
-    // const dateEnd = dateEndRef && dateEndRef.getValue() || null;
-    console.log(date);
+    // The value for the ref for the <select> element is retrieved differently.
+    const type = this.type.current && this.type.current.value;
+    const date = this.getRefValue(this.dateStart);
+    const dateEnd = this.getRefValue(this.dateEnd);
+    const locations = this.getRefValue(this.locations);
+    const paramValues = { type, date, dateEnd, locations };
+
     let url = "/admin/bulk_circulation_events";
-    let params = qs.stringify({ date }, { skipNulls: true });
+    let params = qs.stringify(paramValues, { skipNulls: true });
 
     if (params) {
       url += "?" + params;
     }
 
-    // let link = document.createElement("a");
-    // link.href = url;
-    // link.target = "_blank";
-    // link.click();
+    let link = document.createElement("a");
+    link.href = url;
+    link.target = "_blank";
+    link.click();
   }
 }
--- src/components/__tests__/CirculationEventsDownloadForm-test.tsx
+++ src/components/__tests__/CirculationEventsDownloadForm-test.tsx
@@ -2,30 +2,72 @@ import { expect } from "chai";
 import { stub } from "sinon";
 import { Button } from "library-simplified-reusable-components";
 import * as React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
 import CirculationEventsDownloadForm from "../CirculationEventsDownloadForm";
+import EditableInput from "../EditableInput";
 
-describe("CirculationEventsDownloadForm", () => {
+describe.only("CirculationEventsDownloadForm", () => {
   let wrapper;
   let hide;
 
   beforeEach(() => {
     hide = stub();
-    wrapper = shallow(
+    wrapper = mount(
       <CirculationEventsDownloadForm show={true} hide={hide} />
     );
   });
 
-  it("shows date input", () => {
-    let date = wrapper.find("input[type='date']");
-    expect(date.length).to.equal(1);
+  it("renders a select element", () => {
+    let select = wrapper.find("select");
+    let textInput = wrapper.find("input[name='locations']");
+
+    expect(select.length).to.equal(1);
+    expect(textInput.length).to.equal(0);
   });
 
-  it("shows download and close buttons", () => {
+  it("renders a text input when 'locations' is selected", () => {
+    let select = wrapper.find("select");
+    let textInput = wrapper.find("input[name='locations']");
+
+    expect(textInput.length).to.equal(0);
+    select.getDOMNode().value = "locations";
+    select.simulate("change");
+
+    textInput = wrapper.find("input[name='locations']");
+    expect(textInput.length).to.equal(1);
+
+    select.getDOMNode().value = "all";
+    select.simulate("change");
+
+    textInput = wrapper.find("input[name='locations']");
+    expect(textInput.length).to.equal(0);
+
+    // Manually testing the `toggleLocationsInput` function
+    // without simulation the select element.
+    select.getDOMNode().value = "locations";
+    (wrapper.instance() as any).toggleLocationsInput();
+    wrapper.update();
+
+    textInput = wrapper.find("input[name='locations']");
+    expect(textInput.length).to.equal(1);
+  });
+
+  it("renders start date and end date inputs", () => {
+    let dates = wrapper.find("input[type='date']");
+    expect(dates.length).to.equal(2);
+    expect(dates.at(0).props().name).to.equal("dateStart");
+    expect(dates.at(1).props().name).to.equal("dateEnd");
+  });
+
+  it("renders download and close buttons", () => {
     let buttons = wrapper.find(Button);
     expect(buttons.length).to.equal(2);
     expect(buttons.at(0).prop("content")).to.equal("Download");
     expect(buttons.at(1).prop("content")).to.equal("Close");
+
+    buttons.at(1).simulate("click");
+
+    expect(hide.calledOnce).to.be.true;
   });
 });
