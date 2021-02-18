7ac089b46a4cf1e35caee8de5218e417a8d0d01e
adriana-alter <adrianaalter@nypl.org>
2021-02-18 17:34:42 -0500

    Missed a test

--- src/components/__tests__/ConfigTabContainer-test.tsx
+++ src/components/__tests__/ConfigTabContainer-test.tsx
@@ -123,7 +123,7 @@ describe("ConfigTabContainer", () => {
 
     it("shows components", () => {
       const expectedComponentClasses = [
-        Libraries, IndividualAdmins, AnalyticsServices
+        Libraries, AnalyticsServices
       ];
       for (const componentClass of expectedComponentClasses) {
         const component = wrapper.find(componentClass);
@@ -136,7 +136,7 @@ describe("ConfigTabContainer", () => {
         Collections, AdminAuthServices, PatronAuthServices, SitewideSettings,
         MetadataServices, CDNServices,
         SearchServices, StorageServices, CatalogServices,
-        DiscoveryServices
+        DiscoveryServices, IndividualAdmins
       ];
       for (const componentClass of hiddenComponentClasses) {
         const component = wrapper.find(componentClass);
