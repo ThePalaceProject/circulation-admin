f26c2503906404ffc5163ccb21d931c2e1e41025
Olivia Wong <oliviawong@nypl.org>
2021-10-08 13:56:02 -0400

    Fixes interface for ListManagerContext props

--- src/components/ListManagerContext.tsx
+++ src/components/ListManagerContext.tsx
@@ -6,27 +6,27 @@ import { State } from "../reducers/index";
 export const ListManagerContext = React.createContext(null);
 
 export interface ListManagerProviderProps {
-  children?: JSX.Element[];
+  children?: JSX.Element[] | JSX.Element;
   roles?: {
     role: string;
     library?: string;
   }[];
   email?: string;
-  csrfToken: string;
-  editorStore: Store<State>;
+  csrfToken?: string;
+  editorStore?: Store<State>;
 }
 
-export const ListManagerProvider: React.FC = ({
+export function ListManagerProvider({
   children,
   roles,
   email,
   csrfToken,
   editorStore,
-}: ListManagerProviderProps) => {
+}: ListManagerProviderProps): JSX.Element {
   const admin = new Admin(roles || [], email || null);
   return (
     <ListManagerContext.Provider value={{ admin, csrfToken, editorStore }}>
       {children}
     </ListManagerContext.Provider>
   );
-};
+}
