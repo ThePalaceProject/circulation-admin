df7bd53c6264e99ee61bcaabfbcc0f2e24515f33
Ray Lee <ray.lee@lyrasis.org>
2021-10-14 20:35:10 -0400

    Add npm dev-server script to develop against a remote CM back-end. (#12)
    * Add npm dev-server script to develop against a remote CM back-end.
    
    * Fix a comment typo.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 
 var config = merge(common, {
   mode: "development",
-  devtool: "inline-source-map",
+  devtool: "eval-source-map",
 });
 
 module.exports = config;
