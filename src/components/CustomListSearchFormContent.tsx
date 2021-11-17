641a90fe833c5de541aa948b8ba50724ad79de89
Olivia Wong <oliviawong@nypl.org>
2021-11-17 10:34:30 -0500

    Fixes tests, moves some search logic from parent to child

---
+++ src/components/CustomListSearchFormContent.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import { LanguagesData, LibraryData } from "../interfaces";
+import CustomListSearchAdvancedOptions from "./CustomListSearchAdvancedOptions";
+import EditableInput from "./EditableInput";
+
+export interface CustomListSearchFormContentProps {
+  entryPoints?: string[];
+  setSelectedEntryPoint?: (entryPoint: string) => void;
+  selectedEntryPoint: string;
+  languages: LanguagesData;
+  searchTerms: string;
+  setSearchTerms: (value) => void;
+  sortBy: string | null;
+  setSortBy: (value) => void;
+  selectedLanguage: string;
+  setSelectedLanguage: (value) => void;
+  library: LibraryData;
+}
+
+export default function CustomListSearchFormContent({
+  entryPoints,
+  setSelectedEntryPoint,
+  selectedEntryPoint,
+  languages,
+  searchTerms,
+  setSearchTerms,
+  sortBy,
+  setSortBy,
+  selectedLanguage,
+  setSelectedLanguage,
+  library,
+}: CustomListSearchFormContentProps): JSX.Element {
+  return (
+    <>
+      <fieldset key="search">
+        <legend className="visuallyHidden">Search for titles</legend>
+        {entryPoints && entryPoints.length && (
+          <div className="entry-points">
+            <span>Select the entry point to search for:</span>
+            <div className="entry-points-selection">
+              {!entryPoints.includes("All") && (
+                <EditableInput
+                  key="All"
+                  type="radio"
+                  name="entry-points-selection"
+                  checked={"All" === selectedEntryPoint}
+                  label="All"
+                  value="All"
+                  onChange={() => setSelectedEntryPoint("All")}
+                />
+              )}
+              {entryPoints.map((entryPoint) => (
+                <EditableInput
+                  key={entryPoint}
+                  type="radio"
+                  name="entry-points-selection"
+                  checked={entryPoint === selectedEntryPoint}
+                  label={entryPoint}
+                  value={entryPoint}
+                  onChange={() => setSelectedEntryPoint(entryPoint)}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        <input
+          aria-label="Search for a book title"
+          className="form-control"
+          type="text"
+          placeholder="Enter a search term"
+          value={searchTerms}
+          onChange={(e) => setSearchTerms(e.target.value)}
+        />
+      </fieldset>
+      <CustomListSearchAdvancedOptions
+        key="advancedOptions"
+        languages={languages}
+        library={library}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        selectedLanguage={selectedLanguage}
+        setSelectedLanguage={setSelectedLanguage}
+      />
+    </>
+  );
+}
---
+++ src/components/__tests__/CustomListSearchAdvancedOptions-test.tsx
@@ -0,0 +1,90 @@
+import { expect } from "chai";
+import { stub } from "sinon";
+import * as React from "react";
+import { mount } from "enzyme";
+import CustomListSearchAdvancedOptions from "../CustomListSearchAdvancedOptions";
+
+describe("CustomListSearchAdvancedOptions", () => {
+  let wrapper;
+  let setSortBy;
+  let setSelectedLanguage;
+  const library = {
+    uuid: "uuid",
+    name: "name",
+    short_name: "short_name",
+    settings: {
+      large_collections: ["eng", "fre", "spa"],
+    },
+  };
+
+  const languages = {
+    eng: ["English"],
+    spa: ["Spanish", "Castilian"],
+    fre: ["French"],
+  };
+
+  const selectedLanguage = "all";
+
+  const sortBy = null;
+
+  beforeEach(() => {
+    setSortBy = stub();
+    setSelectedLanguage = stub();
+    wrapper = mount(
+      <CustomListSearchAdvancedOptions
+        library={library}
+        languages={languages}
+        sortBy={sortBy}
+        selectedLanguage={selectedLanguage}
+        setSelectedLanguage={setSelectedLanguage}
+        setSortBy={setSortBy}
+      />
+    );
+  });
+
+  it("displays sort options", () => {
+    const sortOptions = wrapper.find(".search-options").find(".form-group");
+    expect(sortOptions.length).to.equal(3);
+
+    const relevance = sortOptions.at(0);
+    expect(relevance.text()).to.equal("Relevance (default)");
+    const relevanceRadio = relevance.find("input");
+    expect(relevanceRadio.props().type).to.equal("radio");
+    expect(relevanceRadio.props().name).to.be.null;
+    expect(relevanceRadio.props().value).to.equal("");
+    expect(relevanceRadio.props().checked).to.be.true;
+
+    const title = sortOptions.at(1);
+    expect(title.text()).to.equal("Title");
+    const titleRadio = title.find("input");
+    expect(titleRadio.props().type).to.equal("radio");
+    expect(titleRadio.props().name).to.equal("title");
+    expect(titleRadio.props().value).to.equal("title");
+    expect(titleRadio.props().checked).to.be.false;
+
+    const author = sortOptions.at(2);
+    expect(author.text()).to.equal("Author");
+    const authorRadio = author.find("input");
+    expect(authorRadio.props().type).to.equal("radio");
+    expect(authorRadio.props().name).to.equal("author");
+    expect(authorRadio.props().value).to.equal("author");
+    expect(authorRadio.props().checked).to.be.false;
+  });
+
+  it("displays language options", () => {
+    const languageFieldset = wrapper.find("fieldset").at(1);
+    expect(languageFieldset.find("legend").text()).to.equal(
+      "Filter by language:"
+    );
+    const languageMenu = languageFieldset.find("select");
+    const options = languageMenu.find("option");
+    expect(options.at(0).prop("value")).to.equal("all");
+    expect(options.at(0).text()).to.equal("All");
+    expect(options.at(1).prop("value")).to.equal("eng");
+    expect(options.at(1).text()).to.equal("English");
+    expect(options.at(2).prop("value")).to.equal("fre");
+    expect(options.at(2).text()).to.equal("French");
+    expect(options.at(3).prop("value")).to.equal("spa");
+    expect(options.at(3).text()).to.equal("Spanish; Castilian");
+  });
+});
