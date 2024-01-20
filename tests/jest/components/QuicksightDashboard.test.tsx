9589a48a5808e227c4a699c855f6f7de3dc4fd57
Daniel Bernstein <dabylon@gmail.com>
2024-01-19 16:47:44 -0800

    Add new line at end of file.

--- tests/jest/components/QuicksightDashboard.test.tsx
+++ tests/jest/components/QuicksightDashboard.test.tsx
@@ -1,23 +1,29 @@
 import * as React from "react";
-import {screen, waitFor} from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 
 import QuicksightDashboard from "../../../src/components/QuicksightDashboard";
-import {LibrariesData, LibraryData} from "../../../src/interfaces";
-import buildStore, {RootState} from "../../../src/store"
-import {setupServer} from "msw/node";
-import {rest} from "msw";
+import { LibrariesData, LibraryData } from "../../../src/interfaces";
+import buildStore, { RootState } from "../../../src/store";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 import renderWithContext from "../testUtils/renderWithContext";
 
-const libraries: LibrariesData = {libraries: [{uuid: "my-uuid"}]};
+const libraries: LibrariesData = { libraries: [{ uuid: "my-uuid" }] };
 const dashboardId = "test";
-const embedUrl = "http://embedUrl"
-const dashboardUrlData = {embedUrl: embedUrl}
+const embedUrl = "http://embedUrl";
+const dashboardUrlData = { embedUrl: embedUrl };
 
 describe("QuicksightDashboard", () => {
   const server = setupServer(
-      rest.get("*/admin/libraries", (req, res, ctx) => res(ctx.json(libraries))),
+    rest.get("*/admin/libraries", (req, res, ctx) => res(ctx.json(libraries))),
 
-      rest.get("*/admin/quicksight_embed/" + dashboardId + "?libraryUuids=" + libraries["libraries"][0]["uuid"], (req, res, ctx) => res(ctx.json(dashboardUrlData)))
+    rest.get(
+      "*/admin/quicksight_embed/" +
+        dashboardId +
+        "?libraryUuids=" +
+        libraries["libraries"][0]["uuid"],
+      (req, res, ctx) => res(ctx.json(dashboardUrlData))
+    )
   );
 
   beforeAll(() => {
@@ -29,23 +35,18 @@ describe("QuicksightDashboard", () => {
   });
 
   it("embed url is retrieved and set in iframe", async () => {
-
     const contextProviderProps = {
       csrfToken: "",
-      roles: [{role: "system"}],
-
+      roles: [{ role: "system" }],
     };
 
     renderWithContext(
-        <QuicksightDashboard
-            dashboardId={dashboardId}
-            store={buildStore()}
-        />,
-        contextProviderProps
+      <QuicksightDashboard dashboardId={dashboardId} store={buildStore()} />,
+      contextProviderProps
     );
 
     await waitFor(() => {
-      expect(screen.getByRole('iframe')).toHaveAttribute("src", embedUrl)
+      expect(screen.getByRole("iframe")).toHaveAttribute("src", embedUrl);
     });
   });
 });
