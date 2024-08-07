4689d3c938265cbd12c0e0243efa42616a330ac8
Tim DiLauro <tdilauro@users.noreply.github.com>
2024-08-07 13:51:50 -0400

    New tests.

---
+++ tests/jest/context/AppContext.test.tsx
@@ -0,0 +1,72 @@
+import { renderHook } from "@testing-library/react-hooks";
+import {
+  useAppAdmin,
+  useAppContext,
+  useAppEmail,
+  useAppFeatureFlags,
+  useCsrfToken,
+} from "../../../src/context/appContext";
+import { componentWithProviders } from "../testUtils/withProviders";
+import { ContextProviderProps } from "../../../src/components/ContextProvider";
+import { FeatureFlags } from "../../../src/interfaces";
+
+// TODO: These tests may need to be adjusted in the future.
+//  Currently, an AppContext.Provider is injected into the component tree
+//  by the ContextProvider, which itself uses a legacy context API. (See
+//    https://legacy.reactjs.org/docs/legacy-context.html)
+//  but that will change once uses of that API have been removed.
+
+describe("AppContext", () => {
+  const expectedCsrfToken = "token";
+  const expectedEmail = "email";
+  const expectedFeatureFlags: FeatureFlags = {
+    // @ts-expect-error - "testTrue" & "testFalse" aren't valid feature flags
+    testTrue: true,
+    testFalse: false,
+  };
+  const expectedRoles = [{ role: "system" }];
+
+  const contextProviderProps: ContextProviderProps = {
+    csrfToken: expectedCsrfToken,
+    featureFlags: expectedFeatureFlags,
+    roles: expectedRoles,
+    email: expectedEmail,
+  };
+  const wrapper = componentWithProviders({ contextProviderProps });
+
+  it("provides useAppContext context hook", () => {
+    const { result, waitFor } = renderHook(() => useAppContext(), { wrapper });
+    const value = result.current;
+    expect(value.csrfToken).toEqual(expectedCsrfToken);
+    expect(value.admin.email).toEqual(expectedEmail);
+    expect(value.admin.roles).toEqual(expectedRoles);
+    expect(value.featureFlags).toEqual(expectedFeatureFlags);
+  });
+
+  it("provides useAppAdmin context hook", () => {
+    const { result, waitFor } = renderHook(() => useAppAdmin(), { wrapper });
+    const admin = result.current;
+    expect(admin.email).toEqual(expectedEmail);
+    expect(admin.roles).toEqual(expectedRoles);
+  });
+
+  it("provides useAppEmail context hook", () => {
+    const { result, waitFor } = renderHook(() => useAppEmail(), { wrapper });
+    const email = result.current;
+    expect(email).toEqual(expectedEmail);
+  });
+
+  it("provides useCsrfToken context hook", () => {
+    const { result, waitFor } = renderHook(() => useCsrfToken(), { wrapper });
+    const token = result.current;
+    expect(token).toEqual(expectedCsrfToken);
+  });
+
+  it("provides useAppFeatureFlags context hook", () => {
+    const { result, waitFor } = renderHook(() => useAppFeatureFlags(), {
+      wrapper,
+    });
+    const flags = result.current;
+    expect(flags).toEqual(expectedFeatureFlags);
+  });
+});
