808d06983240417d80e6c3ef00f572f4baee9b19
Jonathan Green <jonathan@razzard.com>
2025-04-17 10:35:35 -0300

    Add a doc comment

--- src/utils/NoCacheDataFetcher.ts
+++ src/utils/NoCacheDataFetcher.ts
@@ -1,5 +1,15 @@
 import DataFetcher from "@thepalaceproject/web-opds-client/lib/DataFetcher";
 
+/**
+ * A specialized DataFetcher that prevents caching of HTTP requests.
+ *
+ * This class extends the base DataFetcher and overrides the fetch method to add
+ * no-cache headers and set the cache option to "no-cache". This ensures that
+ * all requests made using this fetcher will not be cached.
+ *
+ * We use this to make sure that admin users always see the latest data when
+ * they are making changes to the system.
+ */
 export default class NoCacheDataFetcher extends DataFetcher {
   fetch(
     url: string,
