5597b8f47f95729fa273b32b50b41cbcf34e31c5
Tim DiLauro <tdilauro@users.noreply.github.com>
2025-07-31 12:07:51 -0400

    Footer text improvements. (PP-2768) (#167)
    ## Description
    
    - Improves contrast of footer text.
    - Provides customizable text labels for footer support contact links:
      - "mailto:" URLs: "Need help? Email `<email-address-from-url>`."
      - other URLs: "Need help?  Contact support."
    - The default can be overridden with the `supportContactText`
    configuration setting: "Need help? `<supportContactText>`"
    
    Note: This PR is ready for review. However, it depends on the refactor
    in #166; ~so, it should not be merged until that one has landed.~
    
    ## Motivation and Context
    
    Improve site UX and increase configurability.
    
    [Jira PP-2768)]
    
    ## How Has This Been Tested?
    
    - Manual testing in local development environment.
    - All tests pass locally.
    - [CI
    tests](https://github.com/ThePalaceProject/circulation-admin/actions/runs/16633036629)
    pass.
    
    ## Checklist:
    
    - N/A - I have updated the documentation accordingly.
    - [x] All new and existing tests passed.

--- src/context/appContext.ts
+++ src/context/appContext.ts
@@ -1,5 +1,9 @@
 import { createContext, useContext } from "react";
-import { DashboardCollectionsBarChart, FeatureFlags } from "../interfaces";
+import {
+  ConfigurationSettings,
+  DashboardCollectionsBarChart,
+  FeatureFlags,
+} from "../interfaces";
 import Admin from "../models/Admin";
 
 export type AppContextType = {
@@ -8,7 +12,7 @@ export type AppContextType = {
   admin: Admin;
   tos_link_text?: string;
   tos_link_href?: string;
-  support_contact_url?: string;
+  supportContact?: SupportContactLink;
   featureFlags: FeatureFlags;
   quicksightPagePath: string;
   dashboardCollectionsBarChart?: DashboardCollectionsBarChart;
@@ -18,6 +22,12 @@ type TermsOfServiceContext = {
   text?: string;
   href?: string;
 };
+export type SupportContactLink = {
+  href: string;
+  text: string;
+};
+
+export const DEFAULT_SUPPORT_CONTACT_TEXT = "Contact support.";
 
 // Don't export this, since we always want the error handling behavior of our hook.
 const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -42,6 +52,43 @@ export const useTermsOfService = (): TermsOfServiceContext => ({
   text: useAppContext().tos_link_text,
   href: useAppContext().tos_link_href,
 });
-export const useSupportContactUrl = () => useAppContext().support_contact_url;
+export const useSupportContact = () => useAppContext().supportContact;
+
+/**
+ * Create a SupportContactLink object from a URL and label text.
+ * If the URL is absent, then we return undefined.
+ * @param supportContactUrl
+ * @param supportContactText
+ * @returns SupportContactLink | undefined
+ */
+export const supportContactLinkFromConfig = ({
+  support_contact_url: deprecatedSupportContactUrl,
+  supportContactUrl: newSupportContactUrl,
+  supportContactText,
+}: Partial<ConfigurationSettings>): SupportContactLink | undefined => {
+  // TODO: Remove `supportContactUrl` here and remove alias above when
+  //  deprecated `support_contact_url` is removed.
+  const supportContactUrl = newSupportContactUrl || deprecatedSupportContactUrl;
+  if (!supportContactUrl) {
+    return undefined;
+  }
+  // If link text is specified, then we can just return that.
+  if (supportContactText) {
+    return {
+      href: supportContactUrl,
+      text: supportContactText,
+    };
+  }
+  // Otherwise, let's try and find a reasonable value for the link text.
+  const { protocol, pathname } = new URL(supportContactUrl);
+  const text =
+    protocol === "mailto:"
+      ? `Email ${pathname}.`
+      : DEFAULT_SUPPORT_CONTACT_TEXT;
+  return {
+    href: supportContactUrl,
+    text: text,
+  };
+};
 
 export default AppContext.Provider;
